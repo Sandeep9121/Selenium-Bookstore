35a376c8a1f4bff546147ad48172eb6f691b86a1
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-06-14 17:31:59 +0100

    [Sandeep] AbstractwebChromeDriver setup

--- bookstore/src/test/java/com/bridgelabz/webdriver/AbstractChromeWebDriver.java
+++ bookstore/src/test/java/com/bridgelabz/webdriver/AbstractChromeWebDriver.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.webdriver;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public abstract class AbstractChromeWebDriver {
 
@@ -15,18 +19,14 @@ public abstract class AbstractChromeWebDriver {
 
 	@BeforeTest
 	public void runSetup() {
-	//		  ChromeDriverManager.getInstance().setup();
-	//		  driver=new ChromeDriver(); 
+			//  ChromeDriverManager.getInstance().setup();
 //String   loginUrl="http://localhost:4200";
 			//chrome Driver manager is giving latest chromeDriver 83 so i downloaded in my system 81 hardcoded the path below
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Mr Sandeep\\Downloads\\chromedriver_win32\\chromedriver.exe"); 
 		driver =new ChromeDriver();
-		//driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-	//driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-		//getting protected driver  from webDriver abstracted class in com.fundoo.webdriver
-		driver.manage().window().maximize();
-		// driver.get("http://localhost:4200");	 
-		 
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+
+		driver.manage().window().maximize();	 
 		 
 		 try {
 			Thread.sleep(1500);
